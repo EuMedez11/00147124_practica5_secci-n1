ccf8b763a44aaae9d5d2638562d302f3e5e0bf5f
EuMedez11 <00147124@uca.edu.sv>
2025-09-30 15:02:19 -0600

    Ejercicios del Labo5

---
+++ Ejercicio_Final/ejerciciofinal/src/Data/scientists.jsx
@@ -0,0 +1,19 @@
+import katsuko from '../assets/Katsuko_Saruhashi.jpg';
+import maria from '../assets/maria.jpg';
+
+export const scientists = [
+  {
+    name: "Maria Sktodowska-curie",
+    image: maria,
+    profession: "fisica y quimica",
+    awards: "4(Premio Nobel de Fisica,cPremio Nobe de quimica, Medalla Dvy,cMedalla Matteucci)",
+    discoveries: "polonio(elemento quimico)"
+  },
+  {
+    name: "Katsuko Saruhashi",
+    image: katsuko,
+    profession: "geoquimica",
+    awards: "2(Premio Miyake de geoquimica, Premio Tanaka)",
+    discoveries: "un metodo para medir el dioxido de carbono en el agua de mar"
+  }
+];
